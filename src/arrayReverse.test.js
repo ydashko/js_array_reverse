75ae97e78ef4e80493340602c1f2fd4a4e4568c0
yaroslavdashko <yaroslav.dashkoa@gmail.com>
2025-07-08 22:38:02 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -1,20 +1,48 @@
 'use strict';
 
-describe(`Function 'arrayReverse':`, () => {
+describe("Function 'arrayReverse':", () => {
   const arrayReverse = require('./arrayReverse');
 
-  it(`should be declared`, () => {
+  it('should be declared', () => {
     expect(arrayReverse).toBeInstanceOf(Function);
   });
 
-  it(`should return an array`, () => {
+  it('should return an array', () => {
+    const result = arrayReverse(['abc']);
 
+    expect(Array.isArray(result)).toBe(true);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
+  it('should reverse a single word', () => {
+    expect(arrayReverse(['Hell0'])).toEqual(['0lleH']);
+  });
+
+  it('should reverse multiple words keeping their lengths', () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it('should handle an array of multiple small words', () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!'])).toEqual([
+      '!',
+      'tn',
+      'e',
+      'dutsamaI',
+    ]);
+  });
+
+  it('should return arr with empty str if array consists an empty string', () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it('should handle empty array', () => {
+    expect(arrayReverse([])).toEqual([]);
+  });
+
+  it('should handle array with multiple empty strings', () => {
+    expect(arrayReverse(['', '', ''])).toEqual(['', '', '']);
+  });
+
+  it('should correctly reverse str with special chars and digits', () => {
+    expect(arrayReverse(['12$', '%^&'])).toEqual(['&^%', '$21']);
+  });
 });
